161970bb1dedc1f0266ffb6da886d7a41516179d
Marwan-G <ghabin2004@gmail.com>
2024-10-25 08:34:38 +0000

    add fun

--- script.js
+++ script.js
@@ -6,31 +6,31 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
+    owner: 'Jonas Schmedtmann',
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2, // %
+    pin: 1111,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
+    owner: 'Jessica Davis',
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
+    owner: 'Steven Thomas Williams',
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 3333,
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
+    owner: 'Sarah Smith',
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 4444,
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -60,51 +60,51 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
+const hideElement = document.querySelector(`.hide`)
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
 const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
+    ['USD', 'United States dollar'],
+    ['EUR', 'Euro'],
+    ['GBP', 'Pound sterling'],
 ]);
 
-const movements  = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 const container = document.querySelector(`.movements`)
-const movments_account = function(movements){
+const movments_account = function (movements) {
 
 
-  movements.map( (mov)=>{
-    const color = mov > 0 ? "movements__type--deposit":"movements__type--withdrawal"
-    const html = `<div class="movements__row">
+    movements.map((mov) => {
+            const color = mov > 0 ? "movements__type--deposit" : "movements__type--withdrawal"
+            const html = `<div class="movements__row">
             <div class="movements__type ${color}">2 deposit</div>
             <div class="movements__date">3 days ago</div>
             <div class="movements__value">${mov}</div>
           </div>`
-     container.insertAdjacentHTML("afterbegin",html)
-    }
-  )
+            container.insertAdjacentHTML("afterbegin", html)
+        }
+    )
 }
 
 
+const createusername = function (accounts) {
 
-const createusername = function(accounts){
+    accounts.forEach(
+        (acc) => {
+            acc.username = acc.owner
+                .split(" ")
+                .map((first) => first.slice(0, 1))
+                .join("").toLocaleLowerCase()
 
-   accounts.forEach(
-    (acc) => {
-    acc.username =acc.owner
-    .split(" ")
-    .map( (first)=> first.slice(0,1))
-    .join("").toLocaleLowerCase()
-
-})
+        })
 
 }
-  
+
 createusername(accounts)
 
 // const diposits = movements.filter( (number)=> number>0)
@@ -121,56 +121,56 @@ createusername(accounts)
 // console.log(balance)
 const balance_value = document.querySelector(`.balance__value`)
 
-const calculate_balance = function(mov){
- return  mov.reduce((acc,curr) => acc+curr)
+const calculate_balance = function (mov) {
+    return mov.reduce((acc, curr) => acc + curr)
 }
 
 
 ///////
 
 const summary = document.querySelector(`.summary__value--in`)
-const incomeBalance = function(movment){
-  return movment.filter( (mov) => mov > 0).reduce((acc,curr)=>acc+curr,0)
+const incomeBalance = function (movment) {
+    return movment.filter((mov) => mov > 0).reduce((acc, curr) => acc + curr, 0)
 }
 
 
-
-
 const summary_out = document.querySelector(`.summary__value--out`)
-const outBalance = function(movment){
-  return movment.filter( (mov) => mov < 0).reduce((acc,curr)=>acc+curr,0)
+const outBalance = function (movment) {
+    return movment.filter((mov) => mov < 0).reduce((acc, curr) => acc + curr, 0)
 }
 
 
 // For each diposit , get interes 1.2
 
 const interset_container = document.querySelector(`.summary__value--interest`)
-const interest = function(movement){
-  return movement.filter( (mov) => mov > 0 )
-  .map((mov) => mov * 1/100)
-  .reduce( (acc, curr) => acc+curr, 0)
+const interest = function (movement) {
+    return movement.filter((mov) => mov > 0)
+        .map((mov) => mov * 1 / 100)
+        .reduce((acc, curr) => acc + curr, 0)
 }
-const intersets =interest(account1.movements)
+const intersets = interest(account1.movements)
 interset_container.textContent = `${intersets}`
 const loginbtn = document.querySelector(`.login__btn`)
 
 let currentAccount;
-loginbtn.addEventListener("click",function(e) {
-  e.preventDefault()  
-    currentAccount = accounts.find((acc) => acc.username === inputLoginUsername.value )
+loginbtn.addEventListener("click", function (e) {
+    e.preventDefault()
+    currentAccount = accounts.find((acc) => acc.username === inputLoginUsername.value)
+
+    hideElement.style.opacity = 100;
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
     movments_account(currentAccount.movements)
     /// account balance
-    const balance =calculate_balance(currentAccount.movements)
-    balance_value.textContent=`${balance}`  
+    const balance = calculate_balance(currentAccount.movements)
+    balance_value.textContent = `${balance}`
     //account income
-    const income =  incomeBalance(currentAccount.movements)
-    summary.textContent=`${income}`
+    const income = incomeBalance(currentAccount.movements)
+    summary.textContent = `${income}`
     //account outbalnace
-    const out =  outBalance(currentAccount.movements)
-    summary_out.textContent=`${Math.abs(out)}`
-  });
-  
-
+    const out = outBalance(currentAccount.movements)
+    summary_out.textContent = `${Math.abs(out)}`
+});
 
 
 // Maximum value of movment array
@@ -183,7 +183,6 @@ loginbtn.addEventListener("click",function(e) {
 // }
 
 //const result = movements.reduce( (max,curr)=> max >= curr? max : curr, movements[0])
-
 
 
 // const dogAge =[1,2,4,6,7,8];
